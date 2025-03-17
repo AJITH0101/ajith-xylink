5b621dfbd47b242494a8d7fee7b74ed6128206c0
Ajith Kumar <ajith.silver@gmail.com>
2025-03-17 21:44:45 +0530

    scroll effect added succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { motion, useAnimation } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import './App.css'
 import Mainbadge from './components/Mainbadge'
 import Hero from './section/Hero'
@@ -8,20 +8,39 @@ import Uplift from './section/Uplift'
 
 
 const App = () => {
+  const [scrollValue, setScrollValue] = useState(100)
+  const [degreeValue, setDegreeValue] = useState(0)
+
+  useEffect(()=>{
+
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const scale = Math.max(40, Math.floor(100 - ((scrollY / 360) * (100 - 40))));
+      const degree = Math.max(-45, Math.floor(0 - ((scrollY / 360) * 45)));
+      console.log("scroll y position", scale);
+      setScrollValue(scale);
+      setDegreeValue(degree)
+      
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  },[])
 
 
 
   
   return (
       <>  
-      <div className="scale-40"  style={{
-    transform: "perspective(1000px) rotateY(-100deg)",
-  }}><Uplift/></div>
-
-
-
-   
-
+<div
+        className="min-h-[150vh] mt-24" // ✅ Only keeping the height
+        style={{
+          transform: `scale(${scrollValue / 100}) perspective(1000px) rotateY(${degreeValue}deg)`,
+          transition: "transform 0.5s ease-out",
+        }}
+      >
+        <Uplift />
+      </div>
     </>
   )
 }
